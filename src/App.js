c5ceef390ea799f188bc6dbd75c0e1bb2bacf8bb
Yeaji <dkswpffk6985@naver.con>
2025-04-01 14:31:06 +0900

    react snap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import MetaTags from "./Assets/MetaTags";
 
 import Main from "./Main/Main";
@@ -17,42 +23,59 @@ import ScrollToTop from "./Assets/ScrollRestoration";
 import RootLayout from "./Root";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: [<RootLayout />, <ScrollToTop />],
-      children: [
-        { index: true, element: <Main /> },
-        {
-          path: "web-design",
-          children: [
-            { index: true, element: <WebDesign /> },
-            { path: ":webId", element: <WebDesignDetail /> },
-          ],
-        },
-        {
-          path: "branding",
-          children: [
-            { index: true, element: <Branding /> },
-            { path: ":brId", element: <BrandingDetail /> },
-          ],
-        },
-        {
-          path: "school-works",
-          children: [
-            { index: true, element: <SchoolWorks /> },
-            { path: ":schoolId", element: <SchoolWorksDetail /> },
-          ],
-        },
-        { path: "resume", element: <Resume /> },
-      ],
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: [<RootLayout />, <ScrollToTop />],
+  //     children: [
+  //       { index: true, element: <Main /> },
+  //       {
+  //         path: "web-design",
+  //         children: [
+  //           { index: true, element: <WebDesign /> },
+  //           { path: ":webId", element: <WebDesignDetail /> },
+  //         ],
+  //       },
+  //       {
+  //         path: "branding",
+  //         children: [
+  //           { index: true, element: <Branding /> },
+  //           { path: ":brId", element: <BrandingDetail /> },
+  //         ],
+  //       },
+  //       {
+  //         path: "school-works",
+  //         children: [
+  //           { index: true, element: <SchoolWorks /> },
+  //           { path: ":schoolId", element: <SchoolWorksDetail /> },
+  //         ],
+  //       },
+  //       { path: "resume", element: <Resume /> },
+  //     ],
+  //   },
+  // ]);
 
   return (
     <>
-      <MetaTags />
-      <RouterProvider router={router} />
+      <HashRouter>
+        <MetaTags />
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Main />} />
+            <Route path="web-design" element={<WebDesign />}>
+              <Route path=":webId" element={<WebDesignDetail />} />
+            </Route>
+            <Route path="branding" element={<Branding />}>
+              <Route path=":brId" element={<BrandingDetail />} />
+            </Route>
+            <Route path="school-works" element={<SchoolWorks />}>
+              <Route path=":schoolId" element={<SchoolWorksDetail />} />
+            </Route>
+            <Route path="resume" element={<Resume />} />
+          </Route>
+        </Routes>
+        <ScrollToTop />
+      </HashRouter>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { HashRouter } from "react-router-dom";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HelmetProvider>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </HelmetProvider>
 );
